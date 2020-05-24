1e15a0532a6fc93746e533ba05dd607ffbdef328
liangxueming <63330167+liangxueming@users.noreply.github.com>
2020-05-24 11:23:42 +0800

    lanmbda V2_all 将list类型抽象化

---
+++ src/main/java/lxm/jdk8/lambda/V2_All.java
@@ -0,0 +1,62 @@
+package lxm.jdk8.lambda;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Predicate  谓语，述语  使……基于；断言；暗示
+ * 将list 类型抽象化  !!! 灵活，简洁
+ */
+public class V2_All {
+    public static void main(String[] args) {
+        List<Apple> checkList = new ArrayList<>();
+        checkList.add( new Apple().setColor("green").setWeight(100));
+        checkList.add( new Apple().setColor("red").setWeight(60));
+        checkList.add( new Apple().setColor("red").setWeight(80));
+
+        filter(checkList,(Apple a)-> "red".equals(a.getColor()));
+
+        List<Integer> numbers = new ArrayList<>();
+        numbers.add(1); numbers.add(2); numbers.add(3); numbers.add(4); numbers.add(5);
+        /**
+         * V1 实现Predicate<Integer>接口
+         */
+        filter(numbers,new Test());
+        /**
+         * V2 匿名类
+         */
+        filter(numbers,new Test() {
+            //方法名必须和Predicate接口里定义的方法名一样
+            public boolean test(Integer i) {
+                return i % 1==0;
+            }
+        });
+        /**
+         * V3 lanmbda
+         */
+        filter(numbers,(Integer i) -> i % 2==0);
+
+    }
+
+    public static <T> List<T> filter(List<T> list, Predicate<T> p) {
+        ArrayList<T> result = new ArrayList<>();
+        for (T e : list) {
+            if(p.test(e)){
+                result.add(e);
+            }
+        }
+        System.out.println(result);
+        return result ;
+    }
+}
+
+interface Predicate<T> {
+    boolean test(T t);
+}
+class Test implements Predicate<Integer>{
+
+    @Override
+    public boolean test(Integer i) {
+        return i % 2==0;
+    }
+}
